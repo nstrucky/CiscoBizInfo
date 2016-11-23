706cbccc1a92844119b4432d6662634e6ab0acc6
nstrucky <nickstruckmeyer@gmail.com>
2016-11-22 19:20:49 -0500

    Initial Commit

---
+++ app/src/main/java/com/netjob/bizinfo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.netjob.bizinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+//comment
+
+
+
+
+
+    }
+}
